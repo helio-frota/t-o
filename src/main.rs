def76df8262dfafafd066b63c7905735b2ab35d1
Helio Frota <00hf11@gmail.com>
2025-01-08 12:56:45 -0300

    custom manual span

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use actix_web::{web, App, HttpRequest, HttpServer};
 
-use tracing::{debug, error, info, span, trace, warn};
+use tracing::{info, span, Level};
 
 async fn hello(_req: HttpRequest) -> &'static str {
+    let span = span!(Level::INFO, "hello function");
+    let _guard = span.enter();
+    info!("before returning Hello");
     "Hello"
 }
 
@@ -11,6 +14,7 @@ async fn main() -> std::io::Result<()> {
     tracing_subscriber::fmt::init();
 
     info!("starting...");
+
     HttpServer::new(|| App::new().service(web::resource("/").to(hello)))
         .bind(("0.0.0.0", 8080))?
         .run()
