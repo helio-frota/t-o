3a05a215066c10135c1eb20556503c6cabc2e6fc
Helio Frota <00hf11@gmail.com>
2025-01-08 12:46:38 -0300

    nothing shows on stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer};
+
+use tracing::{debug, error, info, span, trace, warn};
+
+async fn hello(_req: HttpRequest) -> &'static str {
+    "Hello"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    info!("starting...");
+    HttpServer::new(|| App::new().service(web::resource("/").to(hello)))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
